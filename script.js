de5c3800efa259e7940b9f62cc47c89b1e338bf1
sathya-narine <sathyanarine21@gmail.com>
2024-02-28 17:35:21 -0500

    two video

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-// This code loads the IFrame Player API code asynchronously.
-var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// Variables for the first video
+var player1;
+var clickCount1 = 0;
+var responseSequence1 = [2, 3, 1, 4, 2, 5, 3, 1, 4, 2, 5, 3, 1, 4, 2, 5]; // Longer response sequence
+var index1 = 0;
+var playButton1 = document.getElementById('playButton1');
 
+// Variables for the second video
+var player2;
+var clickCount2 = 0;
+var responseSequence2 = [1, 4, 2, 3, 1, 5, 2, 3, 4, 5, 1, 2, 3, 4, 1, 5]; // Longer response sequence
+var index2 = 0;
+var playButton2 = document.getElementById('playButton2');
 
 // This function creates an <iframe> (and YouTube player) after the API code downloads.
-var player;
 function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
+    // Create player for the first video
+    player1 = new YT.Player('player1', {
         height: '100%', // Set player height
         width: '100%', // Set player width
         videoId: 'M7lc1UVf-VE',
@@ -20,49 +27,113 @@ function onYouTubeIframeAPIReady() {
             'rel': 0 // Disable related videos
         },
         events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
+            'onReady': onPlayerReady1,
+            'onStateChange': onPlayerStateChange1
+        }
+    });
+
+    // Create player for the second video
+    player2 = new YT.Player('player2', {
+        height: '100%', // Set player height
+        width: '100%', // Set player width
+        videoId: 'M7lc1UVf-VE', // Change the videoId for the second video
+        playerVars: {
+            'playsinline': 1,
+            'controls': 0,
+            'autoplay': 0, // Autoplay the video
+            'mute': 0, // Mute the video to avoid autoplay restrictions
+            'rel': 0 // Disable related videos
+        },
+        events: {
+            'onReady': onPlayerReady2,
+            'onStateChange': onPlayerStateChange2
         }
     });
 }
 
-// The API will call this function when the video player is ready.
-function onPlayerReady(event) {
+// The API will call these functions when the video players are ready.
+function onPlayerReady1(event) {
     event.target.setVolume(100); // Set volume to 100 (full volume)
 }
 
-// The API calls this function when the player's state changes.
-// The function indicates that when playing a video (state=1),
-// the player should play for six seconds and then pause.
-var done = false;
-function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.PLAYING && !done) {
-        setTimeout(pauseVideo, 10000);
-        done = true;
+function onPlayerReady2(event) {
+    event.target.setVolume(100); // Set volume to 100 (full volume)
+}
+
+// The API calls these functions when the player's state changes.
+function onPlayerStateChange1(event) {
+    if (event.data == YT.PlayerState.PLAYING) {
+        disableButton(playButton2);
+        setTimeout(function() {
+            enableButton(playButton2);
+        }, 60000); // Enable the button after 1 minute (60000 milliseconds)
+    }
+
+    // Check if the video has ended
+    if (event.data == YT.PlayerState.ENDED && index1 < responseSequence1.length) {
+        // Restart the video from the beginning
+        player1.seekTo(0);
+        player1.playVideo();
     }
 }
 
-function pauseVideo() {
+function onPlayerStateChange2(event) {
+    if (event.data == YT.PlayerState.PLAYING) {
+        disableButton(playButton1);
+        setTimeout(function() {
+            enableButton(playButton1);
+        }, 60000); // Enable the button after 1 minute (60000 milliseconds)
+    }
+
+    // Check if the video has ended
+    if (event.data == YT.PlayerState.ENDED && index2 < responseSequence2.length) {
+        // Restart the video from the beginning
+        player2.seekTo(0);
+        player2.playVideo();
+    }
+}
+
+function disableButton(button) {
+    button.disabled = true;
+}
+
+function enableButton(button) {
+    button.disabled = false;
+}
+
+// Monitor click events on the play buttons for both videos
+playButton1.addEventListener('click', function() {
+    clickCount1++;
+
+    if (clickCount1 === responseSequence1[index1]) {
+        alert("Number of clicks for Video 1 detected: " + clickCount1);
+        player1.playVideo(); // Play the first video when the response sequence is met
+        setTimeout(function() {
+            pauseVideo(player1);
+        }, 60000); // Pause the video after 1 minute (60000 milliseconds)
+        clickCount1 = 0; // Reset click count after playing the video
+        index1++; // Increment the index for the first video
+    }
+});
+
+playButton2.addEventListener('click', function() {
+    clickCount2++;
+
+    if (clickCount2 === responseSequence2[index2]) {
+        alert("Number of clicks for Video 2 detected: " + clickCount2);
+        player2.playVideo(); // Play the second video when the response sequence is met
+        setTimeout(function() {
+            pauseVideo(player2);
+        }, 60000); // Pause the video after 1 minute (60000 milliseconds)
+        clickCount2 = 0; // Reset click count after playing the video
+        index2++; // Increment the index for the second video
+    }
+});
+
+function pauseVideo(player) {
     player.pauseVideo();
 }
 
-// Monitor click events on the container
-var container = document.getElementById('container');
-var clickCount = 0;
-// Define the response sequence for the number of clicks
-var responseSequence = [2, 5, 3, 1, 2, 4];
-var index = 0;
-
-container.addEventListener('click', function() {
-    clickCount++;
-
-    if (clickCount === responseSequence[index]) {
-        alert("NUmber of clicks detected"+clickCount);
-        player.playVideo(); // Play the video when three clicks are detected
-        setTimeout(function() {
-            pauseVideo();
-        }, 10000); // Pause the video after 6 seconds
-        clickCount = 0; // Reset click count after playing the video
-        index++; //increement the index
-    }
-});
+// Update the HTML content with the response sequences for each video
+document.getElementById('responseSequence1').innerHTML = "Response Sequence for Video 1: " + responseSequence1.join(", ");
+document.getElementById('responseSequence2').innerHTML = "Response Sequence for Video 2: " + responseSequence2.join(", ");
